f5d7778430d10fbe97d55741ec1c5d18c1d7a932
xiaopan233 <admin@xiaopan233.club>
2020-03-26 18:10:42 +0800

    v1.0

--- README.md
+++ README.md
@@ -9,19 +9,61 @@
 root权限运行：
 
 ```
-python3 panSubdomainScanner.py xxx.csv xxx.html &#91;thread number&#93; &#91;port&#93; 
+python3 go.py xxx.csv xxx.html [reqThread] [shotThread] [ports]
 ```
 
 其中。xxx.csv为oneforall收集得到的csv
 
 xxx.html为结果输出的html文件
 
-thread number可选，为线程数，默认20
+reqThread可选，为进行网页请求的线程数，默认200
 
-port可选。为要扫描的端口，脚本默认扫描了80和443，如果要新增端口的话就可以写上去（不需要再写80和443），多个端口用英文逗号分开
+shotThread可选，为进行网页截图的线程数，默认30
+
+ports可选。为要扫描的端口，脚本默认扫描了80和443，如果要新增端口的话就可以写上去（不需要再写80和443），多个端口用英文逗号分开
 
 <br>
-本脚本使用了：
+
+运行脚本截图：
+
+![](./image/1.png)
+
+![](./image/2.png)
+
+![](./image/3.png)
+
+其中count为每一个线程的总工作量，now为当前探测到第几个子域名，state为完成状态，subdomain为当前探测的子域名。
+
+<br>
+
+**本脚本有三个阶段：**
+
+第一阶段：调用nmap和requests库探测子域存活和信息手机
+
+第二阶段：调用selenium对存活子域和状态码在200-399的子域进行网页截图
+
+第三阶段：将收集得到的信息整理在一个html里
+
+
+
+<br>
+
+**运行结束后会在当前目录下生成一些文件：**
+
+img文件夹：存放的是网页截图
+
+xxx.html：信息整理好的html文件，使用浏览器打开
+
+error.log：报错日志，可能里面有些子域用上面的工具探测报错的，但实际我们人工访问可以访问
+
+noWebHost.log：使用nmap扫描时，指定的端口没有开放。这里的子域名可以进行一下全端口扫描
+
+responseDictNone.log：使用requests请求时出错了，可能手动访问可以访问。
+
+
+
+<br>
+**本脚本使用了：**
 
 nmap作为存活探测以及端口探测
 
@@ -31,9 +73,9 @@ selenium进行网页截图
 
 <br>
 
-需要先安装好Chrome浏览器
+**需要先安装好Chrome浏览器**
 
-需要安装的库：
+**需要安装的库：**
 
 pip3 install selenium
 
--- panSubdomainScanner.py
+++ panSubdomainScanner.py
@@ -6,172 +6,191 @@ import threading
 import os
 import time
 import math
+import queue
 from selenium import webdriver
 import selenium
+import urllib3
 
-#use: sudo python3 go.py xxx.csv xxx.html 50 8080,8081
+#use: sudo python3 go.py xxx.csv xxx.html 200 30 8080,8081
 os.system("mkdir img")
 
-status = []
+
+running_status = {}
+running_status_a = {}
+req_tmp = []
+result_200 = []
+result_n200 = []
+
+def log_file(filename, content):
+	f = open(filename,"a")
+	f.write(content)
+	f.close()
+
+def log_errors(e, subdomain, port):
+	print("==============================================================")
+	print("log errors")
+	print(e)
+	print("==============================================================")
+
+	localtime = time.strftime("%a %b %d %H:%M:%S %Y", time.localtime())
+	log_file("error.log", localtime + "\n" + e + "\n" + str(port) + "\n"  + subdomain + "\n")
 
 #use nmap scan the hsot
-def pan_scan_port(subdomain):
+def pan_scan_port(subdomain, ports):
 	portOpen = []
-	#default to scan 80 and 443
-	try:
-		ports = "80,443," + sys.argv[4]
-	except:
-		ports = "80,443"
+	noWebFlag = [0,0]
 	
 	nm = nmap.PortScanner()
-	scanResult = nm.scan(subdomain,ports,"-sS -T4")
+	scanResult = nm.scan(subdomain, ports,"-sS -T4")
 
 	#can not be resolved
 	if "error" in scanResult['nmap']['scaninfo'].keys():
 		return {"code" : 0}
 	
 	#get the result
-	scanResult = list(scanResult['scan'].values())[0]['tcp']
-	for info in scanResult.items():
-		if info[1]['state'] == "open":
-			portOpen.append(info[0])
-	return {"code" : 1, "subdomain": subdomain, "portList" : portOpen}
+	try:
+		scanResult = list(scanResult['scan'].values())[0]['tcp']
+		for info in scanResult.items():
+			if info[1]['state'] == "open":
+				portOpen.append(info[0])
+
+			if info[1]['state'] != 'open' and info[0] == 80:
+				noWebFlag[0] = 1
+			if info[1]['state'] != 'open' and info[0] == 443:
+				noWebFlag[1] = 1
+
+		if noWebFlag[0] == noWebFlag[1] == 1:
+			log_file("noWebHost.log", subdomain+"\n")
+
+	except IndexError:
+		pass
+
+	return {"code" : 1, "portOpen" : portOpen}
 
 #send requests to the subdomain. To get isalive,statuCode,title,url
 def pan_uri_response(subdomain,portList):
 	responseDict = {}
-	for port in portList:
-		responseDict[str(port)] = {
-			'http': {},
-			'https' : {}
-		}
 
-		url1 = "http://" + subdomain + ":" + str(port)
-		url2 = "https://" + subdomain + ":" + str(port)
-		try:
-			r1 = requests.get(url=url1,timeout=2)
-		except:
-			r1 = ""
-		try:
-			r2 = requests.get(url=url2,timeout=2)
-		except:
-			r2 = ""
+	if portList != []:
+		for port in portList:
+			responseDict[str(port)] = {}
 
-		if r1 != "":
+			url = "http://" + subdomain + ":" + str(port)
 			try:
-				r1.encoding = r1.apparent_encoding
-				title = r1.text.split("<title>")
-				title = title[1].split("</title>")[0]
+				r = requests.get(url=url,timeout=2)
 			except:
-				title = ""
-			responseDict[str(port)]['http']['title'] = title
-			responseDict[str(port)]['http']['headers'] = r1.headers
-			responseDict[str(port)]['http']['status_code'] = r1.status_code
+				r = ""
 
-		if r1 != r2 and r2 != "":
-			try:
-				r2.encoding = r2.apparent_encoding
-				title = r2.text.split("<title>")
-				title = title[1].split("</title>")[0]
-			except:
-				title = ""
-			responseDict[str(port)]['https']['title'] = title
-			responseDict[str(port)]['https']['headers'] = r2.headers
-			responseDict[str(port)]['https']['status_code'] = r2.status_code
+			if r != "":
+				try:
+					r.encoding = r.apparent_encoding
+					title = r.text.split("<title>")
+					title = title[1].split("</title>")[0]
+				except:
+					title = ""
+				responseDict[str(port)]['title'] = title
+				responseDict[str(port)]['subdomain'] = subdomain
+				responseDict[str(port)]['port'] = port
+				responseDict[str(port)]['headers'] = r.headers
+				responseDict[str(port)]['status_code'] = r.status_code
 
-	return {"code" : 1, 'responseDict' : responseDict}
+	return {"code" : 1, 'subdomain' : subdomain, 'responseDict' : responseDict}
 
 #get the web screen shot
-def pan_screen_shot(driver, subdomain, uri):
-	result = {"subdomain" : subdomain, "main" : []}
-	for port in uri['responseDict'].items():
-		if port[1]['http'] != {}:
-			try:
-				driver.get("http://" + subdomain + ":" + str(port[0]))
-				imgName = "img/pan_" + "http_" + subdomain + "_" + str(port[0]) + ".png"
-				driver.get_screenshot_as_file(r""+imgName)
-				res = {
-					'port' : port[0],
-					'method' : 'http',
-					'title' : port[1]['http']['title'],
-					'headers' : port[1]['http']['headers'],
-					'status_code' : port[1]['http']['status_code'],
-					'imgName' : imgName
-				}
-				result['main'].append(res)
-			except selenium.common.exceptions.TimeoutException:
-				pass
+def pan_screen_shot(driver, subdomain, port, q):
+	try:
+		driver.get(subdomain)
+		imgName = "img/pan_http_" + subdomain + "_" + str(port[0]) + ".png"
+		imgName = imgName.replace("://","_")
+		imgName = imgName.replace(":","_")
+		driver.get_screenshot_as_file(r""+imgName)
+		q.put(imgName)
+	except selenium.common.exceptions.TimeoutException:
+		q.put(1)
+		pass
+	except selenium.common.exceptions.WebDriverException:
+		q.put(1)
+		log_errors("selenium.common.exceptions.WebDriverException", subdomain, str(port))
+	except selenium.common.exceptions.UnexpectedAlertPresentException:
+		q.put(1)
+		log_errors("selenium.common.exceptions.UnexpectedAlertPresentException", subdomain, str(port))
+	except urllib3.exceptions.ProtocolError:
+		q.put(1)
+		log_errors("urllib3.exceptions.ProtocolError", subdomain, str(port))
+	except KeyError:
+		q.put(1)
+		pass
 
-		if port[1]['https'] != {}:
-			try:
-				driver.get("https://" + subdomain + ":" + str(port[0]))
-				imgName = "img/pan_" + "https_" + subdomain + "_" + str(port[0]) + ".png"
-				driver.get_screenshot_as_file(r""+imgName)
-				res = {
-					'port' : port[0],
-					'method' : 'https',
-					'title' : port[1]['https']['title'],
-					'headers' : port[1]['https']['headers'],
-					'status_code' : port[1]['https']['status_code'],
-					'imgName' : imgName
-				}
-				result['main'].append(res)
-			except selenium.common.exceptions.TimeoutException:
-				pass
+def req_to_go(subdomainsList,i):
+	global running_status
+	global req_tmp
 
-	return result
+	running_status[i]['count'] = len(subdomainsList)
+	running_status[i]['now'] = 0
+	running_status[i]['state'] = 0
 
-#write in the html file
-def pan_write_html(result,i):
-	global status
-
-	content = ''
-
-	for res in result:
-		subdomain = res['subdomain']
-		content = content + '<div style="border: 1px solid black;margin: 30px;">'
-
-		for main in res['main']:
-			content = content + '<div class="line">'
-
-			content = content + '<div class="basic">'
-			content = content + '<p>Subdomain: ' + subdomain + '</p>'
-			content = content + '<p>Port: ' + str(main['port']) + '</p>'
-			content = content + '<p>Method: ' + main['method'] + '</p>'
-			content = content + '<p>Title: ' + main['title'] + '</p>'
-			content = content + '<p>Status_code: ' + str(main['status_code']) + '</p>'
-			content = content + '</div>'
-
-			content = content + '<div class="header">'
-			for h_key in main['headers']:
-				content = content + '<p>' + h_key + ':' + main['headers'][h_key] + '</p>'
-			content = content + '</div>'
-
-			content = content + '<div class="img">'
-			content = content + '<img src="' + main['imgName'] + '" width="300px" onclick="showPic(this)">'
-			content = content + '</div>'
-
-			content = content + '</div>'
-
-		content = content + '</div>'
-
-	f = open("./" + str(i) + ".html","w")
-	f.write(content)
-	f.close()
-	status[i] = 1
-
-def to_go(subdomainsList,i):
-	result = []
-	driver = webdriver.Chrome()
-	driver.set_page_load_timeout(6)
 	for subdomain in subdomainsList:
-		portOpen = pan_scan_port(subdomain)
-		if portOpen['code'] == 1:
-			uri = pan_uri_response(portOpen['subdomain'], portOpen['portList'])
-			result.append(pan_screen_shot(driver, subdomain, uri))
-	pan_write_html(result,i)
-	driver.quit()
+		running_status[i]['subdomain'] = subdomain
+		running_status[i]['now'] = running_status[i]['now'] + 1
 
+		try:
+			ports = sys.argv[5]
+		except:
+			ports = "80,443"
+		portOpen = pan_scan_port(subdomain, ports)
+
+		if portOpen['code'] == 1:
+			uri = pan_uri_response(subdomain, portOpen['portOpen'])
+			req_tmp.append(uri)
+
+	running_status[i]['state'] = 1
+
+def shot_to_go(subdomainsList,i):
+	global result_200
+	global result_n200
+
+	running_status_a[i]['count'] = len(subdomainsList)
+	running_status_a[i]['now'] = 0
+	running_status_a[i]['state'] = 0
+
+	driver = webdriver.Chrome()
+	driver.set_page_load_timeout(10)
+	driver.set_script_timeout(10)
+
+	for s in subdomainsList:
+		running_status_a[i]['now'] = running_status_a[i]['now'] + 1
+		running_status_a[i]['subdomain'] = s['subdomain']
+
+		#the result that the port not be opened
+		if s["responseDict"] == {}:
+			log_file("responseDictNone.log", "NULL " + s['subdomain'] + "\n")
+			continue
+
+		#the result that port not be requested successed
+		for code in s["responseDict"].items():
+			if code[1] == {}:
+				log_file("responseDictNone.log", "Port " + s['subdomain'] + "\n")
+			else:
+				if int(code[1]['status_code']) < 200 or int(code[1]['status_code']) > 399:
+					code[1]['imgName'] = ""
+					result_n200.append(code[1])
+				else:
+					q = queue.Queue()
+					code[1]['imgName'] = ""
+					url = "http://" + s['subdomain'] + ":" + str(code[0])
+					threading.Thread(target=pan_screen_shot, args=(driver, url, code[0], q)).start()
+					while True:
+						try:
+							res =  q.get()
+							break
+						except:
+							print("q  time out")
+					if res != 1:
+						code[1]['imgName'] = res
+					result_200.append(code[1])
+
+	driver.quit()
+	running_status_a[i]['state'] = 1
 
 
 subdomainsList = []
@@ -183,47 +202,131 @@ for row in f_csv:
 	else:
 		subdomainsList.append(row[5])
 
-#default 20 thread
+#default 200 and 30 thread
 try:
-	tNumber = int(sys.argv[3])
+	req_tNumber = int(sys.argv[3])
+	shot_tNumber = int(sys.argv[4])
 except:
-	tNumber = 20
+	req_tNumber = 200
+	shot_tNumber = 30
 
 
-if len(subdomainsList) < tNumber:
-	tNumber = len(subdomainsList)
+if len(subdomainsList) < req_tNumber:
+	req_tNumber = len(subdomainsList)
 
-for i in range(tNumber):
-	status.append(0)
-	interval = math.ceil(len(subdomainsList) / tNumber)
+if len(subdomainsList) < shot_tNumber:
+	shot_tNumber = len(subdomainsList)
+
+
+for i in range(req_tNumber):
+	interval = math.ceil(len(subdomainsList) / req_tNumber)
 	if((i*interval) < len(subdomainsList)):
 		subsList = subdomainsList[(i*interval):(i+1)*interval]
-		threading.Thread(target=to_go,args=(subsList,i)).start()
 
+		if not len(subsList) <= 0:
+			running_status[i] = {}
+			threading.Thread(target=req_to_go,args=(subsList,i)).start()
+
+
+#SHOW LOG
 while True:
-	flag = 1
-	for s in status:
-		if s == 0:
-			flag = 0
-	if flag == 1:
-		head = '<!DOCTYPE html><html><head><title>Xiaopan233-scan</title><style type="text/css">.line{width: 100%;display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;}div{padding: 20px 30px;}#shadow1{width:100%;height:100%;position:fixed;left:0;top:0;z-index:998;background-color:black;opacity:0.6;display: none;}#shadow2{width:100%;height:100%;position:fixed;left:0;top:0;z-index:999;display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;display: none;}</style></head><body><div>'
-		f = open(sys.argv[2],"w")
-		f.write(head)
-		j = 0
-		while j<= i:
-			ff = open("./" + str(j) + ".html", "r")
-			content = ff.read()
-			f.write(content)
-			ff.close()
-			j = j + 1
-		foot = '</div><div id="shadow1"></div><div id="shadow2" onclick="showShadow(this)"></div></body></html><script>function showPic(that){var content = document.getElementById("shadow2");var newImg = document.createElement("img");newImg.src = that.src;newImg.id = "newImg";content.appendChild(newImg);document.getElementById("shadow1").style.display = "block";document.getElementById("shadow2").style.display = "block";document.getElementById("shadow2").style = "display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;";} function showShadow(that){var content = document.getElementById("shadow2");var newImg = document.getElementById("newImg");content.removeChild(newImg);document.getElementById("shadow1").style.display = "none";document.getElementById("shadow2").style.display = "none";}</script>'
-		f.write(foot)
-		f.close()
+	DoneFlag = 1
+	for r in running_status.items():
+		print(r)
+		try:
+			if r[1]['state'] == 0:
+				DoneFlag = 0
+		except:
+			DoneFlag = 0
+			print("state warnning!")
+	if DoneFlag == 1:
 		break
-	print("执行状态：")
-	print(status)
 	time.sleep(1)
 
+print("===========================================================")
+print("===========================================================")
+print("=================!!! shot screen time !!!==================")
+print("===========================================================")
+print("===========================================================")
+time.sleep(1)
 
+for i in range(shot_tNumber):
+	interval = math.ceil(len(req_tmp) / shot_tNumber)
+	if((i*interval) < len(req_tmp)):
+		subsList = req_tmp[(i*interval):(i+1)*interval]
 
+		if not len(subsList) <= 0:
+			running_status_a[i] = {}
+			threading.Thread(target=shot_to_go,args=(subsList,i)).start()
+			time.sleep(1)
 
+#SHOW LOG
+while True:
+	DoneFlag = 1
+	for r in running_status_a.items():
+		print(r)
+		try:
+			if r[1]['state'] == 0:
+				DoneFlag = 0
+		except:
+			DoneFlag = 0
+			print("state warnning!")
+	if DoneFlag == 1:
+		break
+	time.sleep(1)
+
+print("====================================================")
+print("WIll be Down")
+print("====================================================")
+
+time.sleep(1)
+
+f = open(sys.argv[2],"w")
+content = ""
+head = '<!DOCTYPE html><html><head><title>Xiaopan233-scan</title><style type="text/css">.line{width: 100%;display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;}div{padding: 20px 30px;}#shadow1{width:100%;height:100%;position:fixed;left:0;top:0;z-index:998;background-color:black;opacity:0.6;display: none;}#shadow2{width:100%;height:100%;position:fixed;left:0;top:0;z-index:999;display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;display: none;}</style></head><body><div>'
+for r in result_200:
+	content = content + ""
+
+	content = content + '<div class="line">'
+
+	content = content + '<div class="basic">'
+	content = content + '<p>Subdomain: ' + r['subdomain'] + '</p>'
+	content = content + '<p>Port: ' + str(r['port']) + '</p>'
+	content = content + '<p>Title: ' + r['title'] + '</p>'
+	content = content + '<p>Status_code: ' + str(r['status_code']) + '</p>'
+	content = content + '</div>'
+
+	content = content + '<div class="header">'
+	for h_key in r['headers'].items():
+		content = content + '<p>' + h_key[0] + ': ' + h_key[1] + '</p>'
+	content = content + '</div>'
+
+	content = content + '<div class="img">'
+	content = content + '<img src="' + r['imgName'] + '" width="300px" onclick="showPic(this)">'
+	content = content + '</div>'
+
+	content = content + '</div>'
+
+for r in result_n200:
+	content = content + ""
+
+	content = content + '<div class="line">'
+
+	content = content + '<div class="basic">'
+	content = content + '<p>Subdomain: ' + r['subdomain'] + '</p>'
+	content = content + '<p>Port: ' + str(r['port']) + '</p>'
+	content = content + '<p>Title: ' + r['title'] + '</p>'
+	content = content + '<p>Status_code: ' + str(r['status_code']) + '</p>'
+	content = content + '</div>'
+
+	content = content + '<div class="header">'
+	for h_key in r['headers'].items():
+		content = content + '<p>' + h_key[0] + ': ' + h_key[1] + '</p>'
+	content = content + '</div>'
+
+	content = content + '</div>'
+
+foot = '</div><div id="shadow1"></div><div id="shadow2" onclick="showShadow(this)"></div></body></html><script>function showPic(that){var content = document.getElementById("shadow2");var newImg = document.createElement("img");newImg.src = that.src;newImg.id = "newImg";content.appendChild(newImg);document.getElementById("shadow1").style.display = "block";document.getElementById("shadow2").style.display = "block";document.getElementById("shadow2").style = "display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;";} function showShadow(that){var content = document.getElementById("shadow2");var newImg = document.getElementById("newImg");content.removeChild(newImg);document.getElementById("shadow1").style.display = "none";document.getElementById("shadow2").style.display = "none";}</script>'
+
+f.write(head + content + foot)
+f.close()
